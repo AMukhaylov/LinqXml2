fc16dadbee5a96762288120f78be418d5e62e57f
Артур Мухайлов <arthurcloud@yandex.ru>
2020-03-02 23:42:30 +0500

    Добавьте файлы проекта.

---
+++ LinqXml2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LinqXml2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = "input.txt";
+            String output = "output.xml";
+            var a = File.ReadAllLines(input, Encoding.GetEncoding("windows-1251"));
+            XDocument d = new XDocument(
+              new XDeclaration(null, "windows-1251", null),
+
+              new XElement("root",
+                a.Select((e, index) => new XElement("line" + (index + 1), e) as XNode)));
+
+            d.Save(output);
+            Process.Start(input);
+            Process.Start(output);
+        }
+    }
+}
